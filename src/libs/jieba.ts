429a3ec1ff13b54dc834b7ecc50970ab41ecaf33
MaxChang3 <maxcfoo@gmail.com>
2025-02-19 01:08:19 +0800

    feat: implement URL and email splitting

--- src/libs/jieba.ts
+++ src/libs/jieba.ts
@@ -1,7 +1,53 @@
 import init, { cut } from 'jieba-wasm'
 
+const URL_REGEX = /(?:http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+(?:[\-.][a-z0-9]+)*\.[a-z]{2,63}(?::\d{1,5})?(?:\/\S*)?/gi
+
+const EMAIL_REGEX = /[a-z0–9._-]+@[a-z0–9.-]+\.[a-z]{2,4}/gi
+
+const URL_EMAIL_REGEX = new RegExp(`${EMAIL_REGEX.source}|${URL_REGEX.source}`, 'gi')
+
+interface TextPart {
+    text: string
+    isURL: boolean
+}
+
+/**
+ * Splits the given text into an array of TextPart objects, separating URLs from non-URL text.
+ *
+ * @param text - The input text to be split.
+ * @returns An array of TextPart objects, where each object represents a segment of the input text.
+ *          Each segment is either a URL or non-URL text, indicated by the `isURL` property.
+ */
+export const splitTextByURL = (text: string): TextPart[] => {
+    const result: TextPart[] = []
+    let match: RegExpExecArray | null
+    let lastIndex = 0
+    // eslint-disable-next-line no-cond-assign
+    while ((match = URL_EMAIL_REGEX.exec(text)) !== null) {
+        if (match.index > lastIndex) { // Add text before URL
+            result.push({
+                text: text.slice(lastIndex, match.index),
+                isURL: false,
+            })
+        }
+        result.push({
+            text: match[0],
+            isURL: true,
+        })
+        lastIndex = match.index + match[0].length
+    }
+    if (lastIndex < text.length) {
+        result.push({
+            text: text.slice(lastIndex),
+            isURL: false,
+        })
+    }
+    return result
+}
+
 class Jieba {
     isReady: boolean
+
     constructor() {
         this.isReady = false
     }
@@ -26,9 +72,15 @@ class Jieba {
         if (text === '') {
             return []
         }
-        return cut(text).filter( // Filter punctuation (exclude hypen) and spaces
-            (word: string) => !word.match(/[\p{P}--\-]|\s/gv),
-        )
+        const textSplitByURL = splitTextByURL(text)
+        const result = (await Promise.all( // Cut text and URL
+            textSplitByURL.map(async ({ text, isURL }) => isURL ? text : cut(text)),
+        ))
+            .flat()
+            .filter( // Filter punctuation (exclude hyphen) and spaces
+                (word: string) => !word.match(/^[\p{P}--\-]|\s$/gv),
+            )
+        return result
     }
 }
 
--- tests/jieba.test.ts
+++ tests/jieba.test.ts
@@ -1,4 +1,4 @@
-import { jieba } from '@/libs/jieba'
+import { jieba, splitTextByURL } from '@/libs/jieba'
 import { expect, it } from 'vitest'
 
 await jieba.init()
@@ -11,4 +11,27 @@ it('should correctly segment Chinese text', async () => {
 it('should correctly segment English text', async () => {
     const result = await jieba.cut('Hello, World!')
     expect(result).toEqual(['Hello', 'World'])
+})
+
+it('should correctly segment mixed text', async () => {
+    const result = await jieba.cut(`1972年，在贝尔实验室成员布莱恩·柯林汉撰写的内部技术文件《A Tutorial Introduction to the Language B》中首次提到了 Hello World 这一字符串。`)
+    expect(result).toEqual(['1972', '年', '在', '贝尔实验室', '成员', '布莱恩', '柯林', '汉', '撰写', '的', '内部', '技术', '文件', 'A', 'Tutorial', 'Introduction', 'to', 'the', 'Language', 'B', '中', '首次', '提到', '了', 'Hello', 'World', '这一', '字符串'])
+})
+
+it('should correctly split text by URL', async () => {
+    const result = splitTextByURL('访问我们的官网example.com时，如果您有任何问题，请发送邮件至service@example.com或通过论坛test.googl联系管理员')
+    expect(result).toEqual([
+        { text: '访问我们的官网', isURL: false },
+        { text: 'example.com', isURL: true },
+        { text: '时，如果您有任何问题，请发送邮件至', isURL: false },
+        { text: 'service@example.com', isURL: true },
+        { text: '或通过论坛', isURL: false },
+        { text: 'test.googl', isURL: true },
+        { text: '联系管理员', isURL: false },
+    ])
+})
+
+it('should correctly segment text with URL and email', async () => {
+    const result = await jieba.cut('访问我们的官网example.com时，如果您有任何问题，请发送邮件至service@example.com或通过论坛test.googl联系管理员')
+    expect(result).toEqual(['访问', '我们', '的', '官网', 'example.com', '时', '如果', '您', '有', '任何', '问题', '请', '发送', '邮件', '至', 'service@example.com', '或', '通过', '论坛', 'test.googl', '联系', '管理员'])
 })
