fdc68b7f0ee3e5a2029fe1170b1df3dfee1ed878
MaxChang3 <maxcfoo@gmail.com>
2025-02-24 20:47:23 +0800

    refactor: simplify import path for segmenter library

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@ import { TextArea } from '@/components/ui/textarea'
 import { ToastAction, Toaster } from '@/components/ui/toast'
 import { useToast } from '@/components/ui/toast/use-toast'
 import { Github, ThemeToggle } from '@/components/widgets'
-import { backendData, type SupportBackend, useSegmenter } from '@/libs/segmenter/index'
+import { backendData, type SupportBackend, useSegmenter } from '@/libs/segmenter'
 import { useClipboard, useDebounceFn, useUrlSearchParams } from '@vueuse/core'
 import { h, ref } from 'vue'
 
